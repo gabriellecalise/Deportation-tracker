516fbad6638b11e22363735b4f3d93e4c38c4a24
gabriellecalise <gcalise@ufl.edu>
2017-04-22 10:52:29 -0400

    First commit
    This is my flask app that I made to track deportations in 6 states

---
+++ deport.py
@@ -0,0 +1,49 @@
+#import flask and makes the templates
+from flask import Flask, render_template, redirect, url_for
+#import your python dict using the variable you made in deportlist.py
+from statelist import STATES
+app = Flask(__name__)
+
+#you have to add this for the web hosting
+application = app
+
+
+
+#this retrieves the ids AND the states from the data and adds it to the list
+def get_ids_and_states(source):
+    ids_and_states = []
+    for row in source:
+        id = row["id"]
+        state = row["state"]
+        ids_and_states.append( [id, state] )
+            #RETURN LINE HAS TO BE TABBED JUST RIGHT. IF IT ISNT INDENTED, THE LOOP WILL NOT WORK. THIS ONE NEEDS TO BE PARALLEL TO THE FOR ROW IN SOURCE LINE
+    return ids_and_states
+
+def get_state(source, id):
+    for row in source:
+        if id == str(row["id"]):
+            state = row["state"]
+            total_deport = row["total_deport"]
+            map = row["map"]
+            id = str(id)
+            #RETURN LINE HAS TO BE TABBED JUST RIGHT. IF IT ISNT INDENTED, THE LOOP WILL NOT WORK. THIS ONE HAS TO BE PARALLEL TO THE ID=STR(ID). I DONT KNOW WHY BUT IT JUST WORKS
+            return id, state, total_deport, map
+
+
+#this is the route for the homepage of the site
+@app.route('/')
+@app.route('/index/')
+def index():
+    ids_and_states = get_ids_and_states(STATES)
+    return render_template('index.html', pairs=ids_and_states)
+
+#route for the detail page for each state
+@app.route('/state/<id>.html/')
+def state(id):
+    id, state, total_deport, map = get_state(STATES, id)
+    return render_template('states.html', state=state, total_deport=total_deport, map=map)
+
+
+#run in debug mode
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ statelist.py
@@ -0,0 +1,9 @@
+#put the python dict into a variable so you can import it into deport.py
+#To make the big numbers look better, put them in quotes to make them a string and then add commas
+STATES = [
+{"id":427336,"state":"Texas","total_deport":"152,427","map":"https://fusiontables.google.com/embedviz?q=select+col0+from+1yONVztlUXEdbLyt9JVH6l2nr31ArSO4k67ulgoIj&viz=MAP&h=false&lat=29.82056721605122&lng=-97.25322846894528&t=1&z=5&l=col0&y=2&tmplt=2&hml=GEOCODABLE"},
+{"id":322016,"state":"California ","total_deport":"26,820","map":"https://fusiontables.google.com/embedviz?q=select+col0+from+1i20_O0HeX0h9J0OpVYGGHFLn-GgNMwMm_0i79YnO&viz=MAP&h=false&lat=34.79951899802847&lng=-114.74896556250002&t=1&z=5&l=col0&y=2&tmplt=2&hml=GEOCODABLE"},
+{"id":352492,"state":"Arizona","total_deport":"26,031","map":"https://fusiontables.google.com/embedviz?q=select+col0+from+1Q4p7qohW9Bi9weyO8_hbTkCvyKKTlBeHIWRXOeHX&viz=MAP&h=false&lat=33.11861581296718&lng=-112.16376215000003&t=1&z=6&l=col0&y=2&tmplt=2&hml=GEOCODABLE"},
+{"id":682334,"state":"Louisiana","total_deport":"14,469","map":"https://fusiontables.google.com/embedviz?q=select+col0+from+1o7re8jz-CcDTvtXw6uWTv2SCCHFg7hvyiiK8M_4g&viz=MAP&amp;h=false&lat=30.686945972192316&lng=-91.44636480000003&t=1&z=8&l=col0&y=2&tmplt=2&hml=GEOCODABLE"},
+{"id":567845,"state":"Georgia","total_deport":"4,954","map":"https://fusiontables.google.com/embedviz?q=select+col0+from+18qYVSmDLTN96FstTtMzp1sqhpGntaA89O5CVd6L0&viz=MAP&h=false&lat=32.4039305933191&lng=-82.95959474999995&t=1&z=7&l=col0&y=2&tmplt=2&hml=GEOCODABLE"},
+{"id":102447,"state":"Florida","total_deport":"4,060","map":"https://fusiontables.google.com/embedviz?q=select+col0+from+1lZcqrLI75sD1TuBHxdvMdVmHSiXHKPoBdN3J35nN&viz=MAP&h=false&lat=28.077532289325998&lng=-81.29724750000003&t=1&z=6&l=col0&y=2&tmplt=2&hml=GEOCODABLE"}];
